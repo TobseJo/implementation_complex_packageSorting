7748d55c74fbb8d7bc694cc43cac77ca37744316
TobseJo <tobi.emmer@t-online.de>
2021-03-03 17:53:30 +0100

    nix

--- src/main/java/Command/ICommand.java
+++ src/main/java/Command/ICommand.java
@@ -1,5 +1,7 @@
 package Command;
 
+import SortingStation.ZS;
+
 public interface ICommand {
     public void execute();
 }
--- src/main/java/Command/InitCommand.java
+++ src/main/java/Command/InitCommand.java
@@ -1,8 +1,9 @@
 package Command;
 
+import configuration.FileReader;
+
 public class InitCommand implements ICommand{
     @Override
     public void execute() {
-
     }
 }
