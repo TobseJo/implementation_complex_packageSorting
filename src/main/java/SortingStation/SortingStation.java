b5c0226b8f68f26653cfe046eb3257ff91003388
Chipsuri1 <luc.thomaier.19@lehre.mosbach.dhbw.de>
2021-03-12 02:15:41 +0100

    added Getter

--- src/main/java/SortingStation/SortingStation.java
+++ src/main/java/SortingStation/SortingStation.java
@@ -34,4 +34,8 @@ public class SortingStation {
     public SortingSystem getSortingSystem() {
         return sortingSystem;
     }
+
+    public ZoneForUnloadingTruck[] getZonesForUnloadingTrucks() {
+        return zonesForUnloadingTrucks;
+    }
 }
